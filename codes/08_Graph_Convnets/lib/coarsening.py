5bab2bbe30f39a7dea43530d12fade5699773ac9
Xavier Bresson <xavier.bresson@gmail.com>
2024-10-18 19:50:54 +0800

    v20241018.0

--- codes/08_Graph_Convnets/lib/coarsening.py
+++ codes/08_Graph_Convnets/lib/coarsening.py
@@ -30,8 +30,8 @@ def rescale_L(L, lmax=2):
     """Rescale Laplacian eigenvalues to [-1,1]"""
     M, M = L.shape
     I = scipy.sparse.identity(M, format='csr', dtype=L.dtype)
-    L /= lmax * 2
-    L -= I
+    # L /= lmax * 2; L -= I
+    L = 2 / lmax * L - I
     return L 
 
 
